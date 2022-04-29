d647b0314fae0fb33ef17053c9f7ac02cdd3e8d6
LukaszJag <lukasz.jagnow@gmail.com>
2022-04-29 09:52:23 +0200

    improve UI , add choice to player,
    add card to hand function, add UI parser, add put card from hand, set funtional structure to work

--- src/Runner.java
+++ src/Runner.java
@@ -40,11 +40,15 @@ public class Runner {
             for (int j = 0; j < humans; j++) {
                 Player hum = new Player(false);
                 players[i] = hum;
+                Deck hand = new Deck();
+                players[i].hand = hand;
                 counter++;
             }
             for (int j = counter; j < playersNum; j++) {
                 Player hum = new Player(true);
                 players[i] = hum;
+                Deck hand = new Deck();
+                players[i].hand = hand;
                 counter++;
             }
         }
@@ -66,7 +70,7 @@ public class Runner {
 
         Card gameCard = new Card(0,'z', true);
 
-        System.out.println("--- Cycle " + cycle + " ---" );
+        System.out.println("--- Cycle " + 1 + " ---" );
 
         while (gameCard.getIsAction()){
             gameCard = mainStack.stack.addCardFromMainStackTop(mainStack);
@@ -86,15 +90,55 @@ public class Runner {
                 players[i].hand.addCardToHand(playerRadomCard);
             }
         }
-                                    /// coding done ///
-        int action;
-        boolean isThereALooser = false;
 
+                                    /// coding done ///
+
+        int action = 0;
+        boolean isThereALooser = false;
+        int currentPlayer = 0;
+        cycle = 0 ;
+        UI gameWindow = new UI();
         // Game Loop
         while(!isThereALooser) {
 
-            action = input.nextInt();
+            cycle++;
+            while (currentPlayer < playersNum) {
 
+                int volumeOfAction = 0;
+
+                boolean correctDecision = false;
+                gameWindow.playerDecisionWindow();
+                while(!(correctDecision)) {
+                    action = input.nextInt();
+                    if (action == 1) {
+                        gameWindow.whichCardFromHand(players[currentPlayer].hand);
+
+                        int cardToPut = input.nextInt();
+                        while (cardToPut > players[currentPlayer].hand.getCardsInDeck()) {
+                            gameWindow.parseTextToLineConsoleText("Wrong input. Try again");
+                            cardToPut = input.nextInt();
+                        }
+                        Card card = new Card(0, 'z', false);
+                        card = players[currentPlayer].hand.putCardFromHand(players[currentPlayer].hand, cardToPut);
+                        players[currentPlayer].putCardOnStack(card, gameCard, players[currentPlayer + 1]);
+
+                    }
+                    if (action == 2) {
+                        players[currentPlayer].addCardToHand(mainStack);
+                    }
+                    if (action == 3) {
+                        players[currentPlayer].showHand();
+                    }
+
+                    if (action == 4) {
+
+                    }
+
+                    if (action == 5) {
+
+                    }
+                }
+            }
         }
     }
 }
--- src/functionality/UI.java
+++ src/functionality/UI.java
@@ -1,7 +1,81 @@
 package functionality;
 
+import javax.sound.sampled.Line;
+
 public class UI {
 
-    //switch ()
+    public int maxTextinformationLength = 70;
+    public int gapBeforeText = 6;
+    public String[] actionToChoice = {"1 - Put card to game stack", "2 - Draw card from main stack", "3 - Have look at cards in your hand", "4 - Check function of action card",
+            "5 - Get amount of card in another players hand"};
 
+
+    public String parseTextToLineConsoleText(String text){
+        String line = "";
+        int gapAfterText;
+
+        line = line + "|";
+
+        for (int i = 0; i < gapBeforeText; i++) {
+            line = line + " ";
+        }
+
+        line = line + text;
+
+        gapAfterText = maxTextinformationLength - line.length() - 1;
+        for (int i = 0; i < gapAfterText; i++) {
+            line = line + " ";
+        }
+        line = line + "|";
+
+        return line;
+    }
+    public void whichCardFromHand(Deck hand){
+        for (int i = 1; i < hand.getCardsInDeck()+1; i++) {
+            System.out.println( i + " - " + hand.cards[i-1].introduceYourself());
+        }
+    }
+    public void playerDecisionWindow() {
+        UI mainUI = new UI();
+        int beforeText;
+        String gap = "";
+
+        for (int i = 0; i < maxTextinformationLength; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+        int lengthOfQuestion = ("|" + "What do you want to do?" +  "|").length();
+        beforeText = maxTextinformationLength - lengthOfQuestion - gapBeforeText;
+
+        // print question before action to choice
+        System.out.print("|");
+        for (int i = 0; i < gapBeforeText; i++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("What do you want to do?");
+
+        for (int i = 0; i < beforeText; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("|");
+
+        System.out.println();
+        for (int i = 0; i < maxTextinformationLength; i++) {
+            System.out.print("-");
+        }
+
+        System.out.println();
+
+        // print action to choice
+        for (int i = 0; i < actionToChoice.length; i++) {
+            System.out.println(mainUI.parseTextToLineConsoleText(actionToChoice[i]));
+        }
+
+        for (int i = 0; i < maxTextinformationLength; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
